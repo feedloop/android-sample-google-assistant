293ed43e971c2096067e20e9cafff07fee3c5024
gusti <gusti.ngurah.mertayasa@gmail.com>
2024-01-09 11:20:30 +0800

    add widget

---
+++ app/src/main/java/ai/co/bri/brimo/AssistantConfig.kt
@@ -0,0 +1,8 @@
+package ai.co.bri.brimo
+
+object AssistantConfig {
+    const val API_KEY = "AIzaSyDz7IFrf1S7cC6ZxYZUme_hymNJ4U7Cw6E"
+    val LANGUAGE_CODE = "en-US"
+    val DEVICE_MODEL_ID = "your_device_model_id_here"
+    val DEVICE_INSTANCE_ID = "your_device_instance_id_here"
+}
