1826f8aecf66b3adcaceab1674a142042180c9e0
gusti <gusti.ngurah.mertayasa@gmail.com>
2024-01-19 11:08:44 +0800

    init

---
+++ app/src/main/java/com/zavanton/appactionsdemo/features/PaymentFragment.kt
@@ -0,0 +1,94 @@
+package com.zavanton.appactionsdemo.features
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.zavanton.appactionsdemo.R
+import com.zavanton.appactionsdemo.databinding.FragmentPaymentBinding
+
+class PaymentFragment : Fragment() {
+
+    companion object {
+
+        private const val MODE = "MODE"
+        private const val VALUE = "VALUE"
+        private const val CURRENCY = "CURRENCY"
+        private const val ORIGIN = "ORIGIN"
+        private const val DESTINATION = "DESTINATION"
+        private const val ORIGIN_PROVIDER = "ORIGIN_PROVIDER"
+        private const val DESTINATION_PROVIDER = "DESTINATION_PROVIDER"
+
+        fun newInstance(
+            transferMode: String,
+            transferValue: String,
+            transferCurrency: String,
+            transferOrigin: String,
+            transferDestination: String,
+            transferOriginProvider: String,
+            transferDestinationProvider: String
+        ): PaymentFragment {
+            return PaymentFragment().also { fragment ->
+                fragment.arguments = Bundle().apply {
+                    putString(MODE, transferMode)
+                    putString(VALUE, transferValue)
+                    putString(CURRENCY, transferCurrency)
+                    putString(ORIGIN, transferOrigin)
+                    putString(DESTINATION, transferDestination)
+                    putString(ORIGIN_PROVIDER, transferOriginProvider)
+                    putString(DESTINATION_PROVIDER, transferDestinationProvider)
+                }
+            }
+        }
+    }
+
+    private lateinit var binding: FragmentPaymentBinding
+
+    private val amount by lazy { arguments?.getString(VALUE).orEmpty() }
+    private val mode by lazy { arguments?.getString(MODE).orEmpty() }
+    private val currency by lazy { arguments?.getString(CURRENCY).orEmpty() }
+    private val origin by lazy { arguments?.getString(ORIGIN).orEmpty() }
+    private val destination by lazy { arguments?.getString(DESTINATION).orEmpty() }
+    private val originProvider by lazy { arguments?.getString(ORIGIN_PROVIDER).orEmpty() }
+    private val destinationProvider by lazy { arguments?.getString(DESTINATION_PROVIDER).orEmpty() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_payment, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val amountFinal = if (amount == "") {
+            "0"
+        } else amount
+
+        val originFinal: String
+        if (origin == "") {
+            originFinal= "-"
+            binding.txtFromRek.text = "-"
+        } else originFinal=origin
+
+        val destinationFinal: String
+        if (destination == "") {
+            destinationFinal= "-"
+            binding.txtToRek.text = "-"
+        } else destinationFinal=destination
+
+        binding.tvAmount.text = amountFinal
+        binding.tvAmount2.text = amountFinal
+        binding.tvAmount3.text = amountFinal
+//        binding.tvMode.text = getString(R.string.transfer_mode, mode)
+        binding.tvCurrency.text = currency
+        binding.tvOrigin.text = originFinal
+        binding.tvDestination.text = destinationFinal
+//        binding.tvOriginProvider.text = getString(R.string.transfer_origin_provider, originProvider)
+//        binding.tvDestinationProvider.text = getString(R.string.transfer_destination_provider, destinationProvider)
+    }
+}
---
+++ app/src/main/java/com/zavanton/appactionsdemo/slices/AccountInfoProvider.kt
@@ -0,0 +1,57 @@
+package com.zavanton.appactionsdemo.slices
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import androidx.core.graphics.drawable.IconCompat
+import androidx.slice.Slice
+import androidx.slice.SliceProvider
+import androidx.slice.builders.*
+import com.zavanton.appactionsdemo.MainActivity
+
+
+class AccountInfoProvider : SliceProvider() {
+
+    companion object {
+
+        const val ACCOUNT_SLICE_AUTHORITY = "com.zavanton.slices.account.provider"
+    }
+
+    /**
+     * This method is called when the Provider is first created,
+     * use it to initialize your code but keep it mind
+     * that you should not do heavy tasks and block the thread.
+     */
+    override fun onCreateSliceProvider(): Boolean {
+        Log.d("zavanton", "zavanton - onCreateSliceProvider")
+        return true
+    }
+
+    /**
+     * When a new request is send to the SliceProvider of this app, this method is called
+     * with the given slice URI.
+     * Here you could directly handle the uri and create a new slice.
+     */
+    override fun onBindSlice(sliceUri: Uri): Slice {
+        Log.d("zavanton", "zavanton - onBindSlice: $sliceUri")
+        // implement logic to get some slice from uri
+        return list(requireNotNull(context), sliceUri, ListBuilder.INFINITY) {
+            header {
+                title = "Saldo anda"
+                subtitle = "Rp 1.500.00"
+                primaryAction = createOpenAppAction()
+            }
+        }
+    }
+
+    private fun createOpenAppAction(): SliceAction {
+        val intent = Intent(context, MainActivity::class.java)
+        return SliceAction.create(
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE),
+            IconCompat.createWithResource(context, R.drawable.abc_ic_star_black_16dp),
+            ListBuilder.SMALL_IMAGE,
+            "Open App"
+        )
+    }
+}
